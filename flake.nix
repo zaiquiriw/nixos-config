527ecc08a16f843e6a87a9be7a6acca14f0d13f1
ZaiquiriW <zaiquiriw@gmail.com>
2024-05-31 13:25:04 -0500

    Setup flake and file structure

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "NixOS configuration with two or more channels";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs @ { self, nixpkgs, nixpkgs-unstable }:
+    let
+      system = "x86_64-linux";
+      overlay-unstable = final: prev: {
+        unstable = import nixpkgs-unstable {
+           inherit system;
+           config.allowUnfree = true;
+        };
+
+      };
+    in {
+      nixosConfigurations = {
+        zephyr = nixpkgs.lib.nixosSystem {
+            inherit system;
+            specialArgs = {inherit inputs;};
+            modules = [
+            # Overlays-module makes "pkgs.unstable" available in configuration.nix
+            ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+            ./hosts/zephyr/common.nix
+            ];
+        };
+        theoreticalHost = nixpkgs.lib.nixosSystem {
+            inherit system;
+            specialArgs = {inherit inputs;};
+            modules = [
+            ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+            ./hosts/theoreticalHost/configuration.nix
+            ];
+        };
+    };
+}
---
+++ hosts/zephyr/configuration.nix
@@ -0,0 +1,24 @@
+# hosts/zephyr/configuration.nix
+
+# Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{ config, lib, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./nixosModules/common.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
