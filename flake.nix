c610298c67355f31a5a74b0061fdaf81d0a78163
ZaiquiriW <zaiquiriw@gmail.com>
2024-06-01 16:48:11 -0500

    Add homemanager standalone and user dir

--- flake.nix
+++ flake.nix
@@ -6,9 +6,14 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
   };
 
-  outputs = inputs @ { self, nixpkgs, nixpkgs-unstable }:
+  outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, home-manager }:
     let
       system = "x86_64-linux";
       overlay-unstable = final: prev: {
@@ -35,6 +40,16 @@
           modules = [
             ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
             ./hosts/theoreticalHost/configuration.nix
+          ];
+        };
+      };
+
+      homeConfigurations = {
+        zaiq = home-manager.lib.homeManagerConfiguration {
+          inherit system;
+          specialArgs = {inherit inputs;};
+          modules = [
+            ./users/zaiq/home.nix
           ];
         };
       };
--- hosts/zephyr/configuration.nix
+++ hosts/zephyr/configuration.nix
@@ -11,13 +11,23 @@
       ../../nixosModules
     ];
 
+  # Test if vm works and git is installed
+  users.users.zaiq = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    initialPassword = "test";
+  };
+
+  # Copy config folder into etc of the new system
+  environment.etc."nixos-config".source = ../..;
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
   # and migrated your data accordingly.
-  #
+  
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05"; # Did you read the comment?
 
