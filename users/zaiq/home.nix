6532b244d26f2c6cf13d96226ed9ed06b8a39e93
ZaiquiriW <zaiquiriw@gmail.com>
2024-06-01 16:48:11 -0500

    Add homemanager standalone and user dir

---
+++ users/zaiq/home.nix
@@ -0,0 +1,24 @@
+# users/zaiq/home.nix
+# Contains some basic home-manager configuration
+{ config, pkgs, ... }:
+
+{
+    home.username = "zaiq";
+    home.homeDirectory = "/home/zaiq";
+  
+    home.packages = [
+        pkgs.neovim
+        nixpkgs.unstable.git
+    ];
+
+    home.sessionVariables = {
+        EDITOR = "nvim";
+    };
+
+    home.file = {
+        # Add some custom files here
+    }
+
+    home.stateVersion = "24.05";
+    programs.home-manager.enable = true;
+}
