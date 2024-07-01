cce2dcb25b22f050b56b75d17fa15fd29bf5c68d
ZaiquiriW <zaiquiriw@gmail.com>
2024-07-01 12:14:16 -0500

    Final wezterm for now

--- users/zaiq/wezterm.lua
+++ users/zaiq/wezterm.lua
@@ -1,9 +1,19 @@
+local wezterm = require 'wezterm'
+
 local config = {}
 
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 16.0
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = .8
+config.window_background_opacity = .9
 config.color_scheme = 'Gruvbox dark, hard (base16)'
+
+config.keys = {
+  {
+    key = 'r',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.ReloadConfiguration,
+  },
+}
 
 return config
