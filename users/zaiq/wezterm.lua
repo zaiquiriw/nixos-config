98bfcb31bd9a3ef0990c9d85efcfb71cf3a764f3
ZaiquiriW <zaiquiriw@gmail.com>
2024-07-01 12:16:20 -0500

    JK clean up

--- users/zaiq/wezterm.lua
+++ users/zaiq/wezterm.lua
@@ -1,5 +1,3 @@
-local wezterm = require 'wezterm'
-
 local config = {}
 
 config.font = wezterm.font("FiraCode Nerd Font")
@@ -7,13 +5,5 @@ config.font_size = 16.0
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = .9
 config.color_scheme = 'Gruvbox dark, hard (base16)'
-
-config.keys = {
-  {
-    key = 'r',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ReloadConfiguration,
-  },
-}
 
 return config
