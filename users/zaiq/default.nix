830e0a283d3a6a08238927e6d65288ae63cc75ec
ZaiquiriW <zaiquiriw@gmail.com>
2024-07-01 09:43:21 -0500

    Add transparency

--- users/zaiq/default.nix
+++ users/zaiq/default.nix
@@ -66,6 +66,7 @@
         config.font = wezterm.font("FiraCode Nerd Font")
         config.font_size = 16.0
         config.hide_tab_bar_if_only_one_tab = true
+	config.window_background_opacity = .9
 
         return config
 
