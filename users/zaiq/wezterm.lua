37467779bbfc3d3081144d419ed80a61a0e930be
ZaiquiriW <zaiquiriw@gmail.com>
2024-07-01 11:04:35 -0500

    Set wezterm to gruvbox dark

--- users/zaiq/wezterm.lua
+++ users/zaiq/wezterm.lua
@@ -4,5 +4,6 @@ config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 16.0
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = .8
+config.color_scheme = 'Gruvbox dark, hard (base16)'
 
 return config
