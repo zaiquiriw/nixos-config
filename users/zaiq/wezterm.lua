d41180e6e757cc02b4bf538aeec7ce6c892fa365
ZaiquiriW <zaiquiriw@gmail.com>
2024-07-01 18:44:02 -0500

    Add wezterm requirement for hot reload

--- users/zaiq/wezterm.lua
+++ users/zaiq/wezterm.lua
@@ -1,3 +1,5 @@
+local wezterm = require 'wezterm'
+
 local config = {}
 
 config.font = wezterm.font("FiraCode Nerd Font")
@@ -5,5 +7,18 @@ config.font_size = 16.0
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = .9
 config.color_scheme = 'Gruvbox dark, hard (base16)'
+
+config.keys = {
+  {
+    key = 'C',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.CopyTo 'ClipboardAndPrimarySelection',
+  },
+  {
+    key = 'V',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.PasteFrom 'Clipboard'
+  },
+}
 
 return config
