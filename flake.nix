c166103d616230c5556823bbe7ebe941556565f0
ZaiquiriW <zaiquiriw@gmail.com>
2024-07-01 23:40:48 -0500

    Add unstable input, unused

--- flake.nix
+++ flake.nix
@@ -10,6 +10,11 @@
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
     home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager-unstable = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
