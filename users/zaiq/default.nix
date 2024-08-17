976065507674efc3ea632641df71d93ecc9beb12
ZaiquiriW <zaiquiriw@gmail.com>
2024-08-17 12:35:55 -0500

    Add unstable overlay to zaiq

--- users/zaiq/default.nix
+++ users/zaiq/default.nix
@@ -1,24 +1,53 @@
 # users/zaiq/home.nix
-# Contains some basic home-manager configuration
-{ config, pkgs, ... }:
+
+# This file is only for user info, and importing in all the other supporting
+# configuration for this user. Other packages and applications will be imported
+# and set in separate files (or folders if warranted). The user's configuration
+# should be explicitly things the user may want to have on every system they
+# use, settings that feel personal, and are somewhat separate from the host's
+# core purpose or functionality.
+
+# Purpose of inputs:
+# - config:  
+# - pkgs:    
+# - inputs:  
+# - outputs: 
+# - lib:     
+{ config, pkgs, inputs, outputs, lib, ... }:
 
 {
+    # Basic user information
     home.username = "zaiq";
     home.homeDirectory = "/home/zaiq";
+
+    # Configure nixpkgs to use the overlays, like the unstable version set up
+    # in the flake. As well as any other nixpkgs config
+    nixpkgs = {
+        overlays = [
+	    outputs.overlays.unstable-packages
+	];
+	config = {
+             # allowUnfree = true;
+	};
+    };
   
+    # TODO Move to general packages file, with notes on files per application.
     home.packages = [
         pkgs.neovim
         pkgs.git
     ];
 
+    # TODO Transport to a console management file
     home.sessionVariables = {
         EDITOR = "nvim";
     };
 
+    # Have custom setting inputs per application, or in a central file
     home.file = {
         # Add some custom files here
     };
 
+    # Don't change, enables home-manager and maintains defaults for critical app
     home.stateVersion = "24.05";
     programs.home-manager.enable = true;
 }
