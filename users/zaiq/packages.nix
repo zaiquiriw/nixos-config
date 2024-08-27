409c7179993be411b70ad35491d39c296dde1c94
ZaiquiriW <zaiquiriw@gmail.com>
2024-08-27 00:26:00 -0500

    Add pandoc

--- users/zaiq/packages.nix
+++ users/zaiq/packages.nix
@@ -2,5 +2,10 @@
   home.packages = [
     pkgs.obsidian
     pkgs.discord
+    pkgs.libreoffice-qt
+    pkgs.hunspell
+    pkgs.hunspellDicts.en-us-large
+    pkgs.pandoc
+    pkgs.tectonic-unwrapped
   ];
 }
