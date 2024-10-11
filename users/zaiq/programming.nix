7cd2ccc5ab4c62f110db427ffac44300dbd325a7
ZaiquiriW <zaiquiriw@gmail.com>
2024-10-11 14:19:19 -0500

    Add python to starship?

--- users/zaiq/programming.nix
+++ users/zaiq/programming.nix
@@ -18,17 +18,7 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
-    # Configuration written to ~/.config/starship.toml
-    settings = {
-      # add_newline = false;
-
-      # character = {
-      #   success_symbol = "[➜](bold green)";
-      #   error_symbol = "[➜](bold red)";
-      # };
-
-      # package.disabled = true;
-    };
+    settings = builtins.fromTOML (builtins.readFile ./starship.toml);
   };
 
   fonts.fontconfig.enable = true;
