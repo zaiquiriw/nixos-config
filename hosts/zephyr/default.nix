2cac69caba26048068121412cf40074be69c90a3
ZaiquiriW <zaiquiriw@gmail.com>
2024-10-12 14:15:24 -0500

    Remove nix prompt text

--- hosts/zephyr/default.nix
+++ hosts/zephyr/default.nix
@@ -19,6 +19,15 @@
   #-------------#
   # SET OPTIONS #
   #-------------# 
+  
+  nixpkgs.config.allowUnfree = true;
+
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 
   graphics.enable = true;
   hardware.pulseaudio.enable = false;
--- users/zaiq/default.nix
+++ users/zaiq/default.nix
@@ -29,6 +29,8 @@
 
     browsers.enable = true;
 
+    
+
     #-------------#
     # USER CONFIG #
     #-------------#
